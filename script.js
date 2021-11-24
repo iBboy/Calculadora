60046a175d728f973ea0b4779ebb38c8b9686585
Leonardo Rolim <leonardo.skil001@gmail.com>
2021-11-24 20:07:41 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,24 @@
+let numero = document.querySelector("#resultado").innerHTML
+
+function insert(num) {
+  document.querySelector('#resultado').innerHTML += num
+}
+
+function clean() {
+  document.querySelector('#resultado').innerHTML = ""
+}
+
+function back() {
+  var resultado = document.querySelector('#resultado').innerHTML
+    document.querySelector('#resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+  var resultado = document.querySelector('#resultado').innerHTML
+
+  if(resultado){
+    document.querySelector('#resultado').innerHTML = eval(resultado)
+  } else {
+    document.querySelector('#resultado').innerHTML = "???"
+  }
+}
